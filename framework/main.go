3bca649f6d5b113abbb2fba6e1d933ce92259c3a
Serdar Raşit Fırlayiş <rasit.firlayis@tubitak.gov.tr>
2024-06-27 13:58:52 +0300

    :sparkles: feat: DELETE method section completed

--- framework/main.go
+++ framework/main.go
@@ -28,14 +28,14 @@ func main() {
 
 	e := echo.New()
 	v := validator.New()
-	proucts := []map[int]string{{1: "mobile"}, {2: "tv"}, {3: "laptop"}}
+	products := []map[int]string{{1: "mobile"}, {2: "tv"}, {3: "laptop"}}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
 	e.GET("/products", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, proucts)
+		return c.JSON(http.StatusOK, products)
 	})
 
 	e.GET("/products/:id", func(c echo.Context) error {
@@ -44,7 +44,7 @@ func main() {
 		if err != nil {
 			return err
 		}
-		for _, p := range proucts {
+		for _, p := range products {
 			for k := range p {
 				if pID == k {
 					product = p
@@ -55,6 +55,32 @@ func main() {
 		if product == nil {
 			return c.JSON(http.StatusNotFound, "Product not found")
 		}
+		return c.JSON(http.StatusOK, product)
+	})
+
+	e.DELETE("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		var index int
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
+		for i, p := range products {
+			for k := range p {
+				if pID == k {
+					product = p
+					index = i
+				}
+			}
+		}
+
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "Product not found")
+		}
+		splice := func(s []map[int]string, index int) []map[int]string {
+			return append(s[:index], s[index+1:]...)
+		}
+		products = splice(products, index)
 		return c.JSON(http.StatusOK, product)
 	})
 
@@ -73,8 +99,8 @@ func main() {
 		/* if err := v.Struct(reqBody); err != nil {
 			return err
 		} */
-		product := map[int]string{len(proucts) + 1: reqBody.Name}
-		proucts = append(proucts, product)
+		product := map[int]string{len(products) + 1: reqBody.Name}
+		products = append(products, product)
 		return c.JSON(http.StatusOK, product)
 	})
 
@@ -84,7 +110,7 @@ func main() {
 		if err != nil {
 			return err
 		}
-		for _, p := range proucts {
+		for _, p := range products {
 			for k := range p {
 				if pID == k {
 					product = p
